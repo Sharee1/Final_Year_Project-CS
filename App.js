2ef288257b2a6440ec07e6f61aa01c2c240af2c3
Shaheer Majid <shaheermajid2002@gmail.com>
2023-10-25 13:39:19 +0500

    First Version

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './components/login';
+import Signup from './components/signup';
+
+
+
+export default function App() {
+  return (
+    <View >
+
+<Login/>
+     <Text>
+      
+
+
+     </Text>
+  
+
+    </View>
+  );
+}
+
+
