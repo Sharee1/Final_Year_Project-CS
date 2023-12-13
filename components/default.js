ab44986a9d50589384ba36e1315f92be031f08d2
Shaheer Majid <shaheermajid2002@gmail.com>
2023-12-13 16:39:45 +0500

    new Features UX UI

---
+++ components/default.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const HomePage = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Your Meal Planner</Text>
+
+      <View style={styles.buttonContainer}>
+        {}
+        <Button
+          title="Search Recipe"
+          onPress={() => navigation.navigate('SearchRecipe')}
+          color="#FFD700" 
+        />
+      </View>
+
+      {}
+      <View style={styles.buttonSpacer} />
+
+      <View style={styles.buttonContainer}>
+        {}
+        <Button
+          title="Substitute"
+          onPress={() => navigation.navigate('Substitute')}
+          color="#FFD700" 
+        />
+      </View>
+
+      {}
+      <View style={styles.buttonSpacer} />
+
+      <View style={styles.buttonContainer}>
+        {}
+        <Button
+          title="Weekly Schedule"
+          onPress={() => navigation.navigate('WeeklySchedule')}
+          color="#FFD700" 
+        />
+      </View>
+
+      {}
+      <View style={styles.buttonSpacer} />
+
+      <View style={styles.buttonContainer}>
+        {/* Button 4: Scan Your Meal */}
+        <Button
+          title="Scan Your Meal"
+          onPress={() => navigation.navigate('ScanYourMeal')}
+          color="#FFD700" // Yellow color
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: 'White', // Yellow background
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 140,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 10, // Added marginBottom for spacing
+  },
+  buttonSpacer: {
+    height: 20, // Adjusted height for vertical spacing
+  },
+});
+
+export default HomePage;
